de353f0ca14e06b46cbca09933c03f4507afa401
jinieeee <lje911@gmail.com>
2023-12-30 22:50:20 +0900

    [feat] tailwind css 적용

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -8,11 +8,31 @@ function Login() {
     }
 
     return (
-        <div>
-            <h1>소셜 로그인</h1>
-            <a onClick={kakaoLogin}>
-                <img src="/img/login/kakao_login_medium_wide.png"/>
-            </a>
+        <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+            <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+                <img
+                    className="mx-auto h-10 w-auto"
+                    src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+                    alt="Your Company"
+                />
+                <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+                    Sign in to your account
+                </h2>
+            </div>
+            <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+                <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+                    <h6 className="mb-5 text-center">continue with</h6>
+                </div>
+                <div>
+                <a onClick={kakaoLogin}>
+                    <img
+                        className="mx-auto h-10 w-auto"
+                        src="/img/login/kakao_login_medium_wide.png"
+                        alt="Kakao Login"/>
+                </a>
+            </div>
+            </div>
+
         </div>
     )
 }
